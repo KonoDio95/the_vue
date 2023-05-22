c63f550d1563174a379a1caafc80f69328324382
KonoDio95 <111469547+KonoDio95@users.noreply.github.com>
2023-05-22 11:28:48 -0300

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,20 +18,20 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/examples',
-      name: 'Exemplos',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ExamplesView.vue')
-    },
-    {
       path: '/community',
       name: 'Comunidade',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ComunityView.vue')
+    },
+    {
+      path: '/examples',
+      name: 'Exemplos',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ExamplesView.vue')
     },
     {
       path: '/differences',

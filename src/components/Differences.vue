d690a72f309b9ad8ca951307c75bc635361d7e1d
KonoDio95 <111469547+KonoDio95@users.noreply.github.com>
2023-05-22 11:19:16 -0300

    Add files via upload

---
+++ src/components/Differences.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Exemplos</h1>
+    Certamente! O Vue.js tem sido amplamente adotado e utilizado em diversos projetos reais. Aqui estão alguns exemplos notáveis de sites e aplicativos que foram desenvolvidos com Vue.js:
+
+<ul>
+    <li>
+        Alibaba - Uma das maiores empresas de comércio eletrônico do mundo, a Alibaba, utiliza o Vue.js em suas aplicações internas e externas para fornecer uma experiência de usuário rápida e responsiva.
+    </li>
+    <li>
+        Xiaomi - A fabricante chinesa de eletrônicos Xiaomi utiliza o Vue.js em seu site oficial para criar uma experiência de compra suave e interativa.
+    </li>
+    <li>
+        Baidu - O Baidu, o principal motor de busca da China, adotou o Vue.js para o desenvolvimento de várias de suas ferramentas e serviços.
+    </li>
+    <li>
+        Xiaomi MIUI - A interface de usuário personalizada da Xiaomi para seus smartphones, conhecida como MIUI, é desenvolvida com Vue.js. Ela oferece uma experiência fluida e rica em recursos.
+    </li>
+    <li>
+        Laravel Nova - O Laravel Nova, uma ferramenta de administração do Laravel, utiliza o Vue.js para construir sua interface de usuário intuitiva e responsiva.
+    </li>
+    <li>
+        Trustpilot - A plataforma de avaliações de consumidores Trustpilot incorpora o Vue.js para fornecer uma experiência de usuário moderna e interativa em seu site.
+    </li>
+    <li>
+        GitLab - O GitLab, uma plataforma de gerenciamento de projetos e controle de versão baseada em Git, utiliza o Vue.js em sua interface de usuário para criar uma experiência de trabalho colaborativa e eficiente.
+    </li>
+</ul>
+
+Esses são apenas alguns exemplos de projetos reais que utilizam o Vue.js. Essa ampla adoção em empresas renomadas demonstra a confiança e a eficácia do Vue.js como uma escolha sólida para o desenvolvimento de sites e aplicativos.
+</template>

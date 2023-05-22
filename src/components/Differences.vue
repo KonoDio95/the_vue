e3e528c53178fd753cad89abe872eae84d3899f4
KonoDio95 <111469547+KonoDio95@users.noreply.github.com>
2023-05-22 12:00:33 -0300

    Add files via upload

--- src/components/Differences.vue
+++ src/components/Differences.vue
@@ -1,4 +1,95 @@
 <template>
     <h1>Diferenças</h1>
+    <ul>
+        <li>
+            <strong>Abordagem de renderização:</strong>
+            <p>
+                O Vue.js utiliza uma renderização reativa baseada em template, onde o
+                DOM é atualizado automaticamente quando os dados do componente são
+                modificados. O React, por outro lado, utiliza uma abordagem de
+                renderização virtual (Virtual DOM), onde as atualizações no DOM são
+                otimizadas por meio de uma comparação eficiente de diferenças entre as
+                versões antiga e nova do DOM.
+            </p>
+        </li>
+        <li>
+            <strong>Tamanho da biblioteca:</strong>
+            <p>
+                O Vue.js é consideravelmente menor em tamanho em comparação com o
+                Angular e o React. O Vue.js é projetado para ser modular e permite que
+                você importe apenas as partes necessárias, o que pode resultar em um
+                pacote menor para o cliente final.
+            </p>
+        </li>
+        <li>
+            <strong>Curva de aprendizado:</strong>
+            <p>
+                O Vue.js é frequentemente considerado mais fácil de aprender e usar em
+                comparação com o Angular e o React. Sua sintaxe intuitiva e abordagem
+                progressiva tornam a curva de aprendizado mais suave, especialmente para
+                desenvolvedores iniciantes.
+            </p>
+        </li>
+        <li>
+            <strong>Flexibilidade:</strong>
+            <p>
+                O Vue.js é altamente flexível e permite que você escolha diferentes
+                estilos de desenvolvimento. Ele não impõe uma arquitetura específica e
+                permite que você use bibliotecas e ferramentas adicionais de acordo com
+                suas necessidades. O Angular e o React, por outro lado, são mais
+                prescritivos em sua abordagem e geralmente possuem uma estrutura mais
+                rígida.
+            </p>
+        </li>
+    </ul>
+
+     
     <h1>Similaridades</h1>
+    <ul>
+        <li>
+            <strong>Componentização:</strong>
+            <p>
+                Todos os três frameworks - Vue.js, React e Angular - adotam o conceito
+                de componentização, onde as interfaces de usuário são divididas em
+                componentes reutilizáveis e independentes. Isso facilita a manutenção do
+                código, o reuso e a construção de interfaces complexas.
+            </p>
+        </li>
+
+        <li>
+            <strong>Uso de JavaScript:</strong>
+            <p>
+                Todos os três frameworks utilizam JavaScript como linguagem principal
+                para o desenvolvimento. Eles também suportam a escrita de código em JSX
+                (React) ou TypeScript (Angular, Vue.js) para adicionar recursos
+                adicionais à linguagem.
+            </p>
+        </li>
+        <li>
+            <strong>Suporte à comunidade:</strong>
+            <p>
+                Tanto o Vue.js quanto o React e o Angular têm comunidades ativas e
+                grandes ecossistemas de desenvolvedores. Existem bibliotecas, plugins e
+                ferramentas de suporte disponíveis para cada um deles, o que facilita o
+                desenvolvimento de aplicativos em larga escala.
+            </p>
+        </li>
+        <li>
+            <strong>Reatividade:</strong>
+            <p>
+                Embora a implementação possa variar, todos os três frameworks possuem
+                recursos que permitem a reatividade em seus componentes. Isso significa
+                que as visualizações são automaticamente atualizadas quando os dados
+                subjacentes são modificados.
+            </p>
+        </li>
+    </ul>
+
+    <p>
+        É importante lembrar que essas são apenas algumas das diferenças e
+        similaridades entre o Vue.js e outros frameworks. A escolha do framework
+        mais adequado para um projeto específico depende das necessidades do
+        projeto, das preferências da equipe de desenvolvimento e do ecossistema em
+        torno do framework.
+    </p>
 </template>
